6e9abdb481c48ed529e350a70f07df2b3080f0f8
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-08 15:19:59 +0900

    게임 완성

--- CPU_Kingdom/pushpull.c
+++ CPU_Kingdom/pushpull.c
@@ -3,14 +3,18 @@
 #include<windows.h>
 
 // _getch() value of ESC key
+#define MAGIC_KEY 224
 #define ESC 27
 #define A 65
 #define a 97
 #define L 76
 #define l 108
+#define ENTER 13
 
-void start_game(void);
+void menu_draw(void);
 void game_fun(void);
+void start_game(void);
+
 char key;
 char t_name1[20];
 char t_name2[20];
@@ -19,55 +23,153 @@ int y = 11;
 int win = 0;
 int win_lose = 0;
 
-void pushpull(void) {
-	char t_name1;
-	char t_name2;
-	system("cls"); 
+enum MENU
+{
+	GAMESTART = 0,
+	RULE,
+	EXIT
+};
 
-	do {
-		gotoxy(37, 5);
-		printf("★★★★★★★★★★ 줄다리기 ★★★★★★★★★★\n");
-		gotoxy(37, 6);
-		printf("★");
-		gotoxy(85, 6);
-		printf("★");
-		gotoxy(37, 7);
-		printf("★         A키와 L키를 마구마구 연타!!          ★\n");
-		gotoxy(37, 8);
-		printf("★ 줄을 가장 빨리 자기쪽으로 당기는 게임입니다! ★\n");
-		gotoxy(37, 9);
-		printf("★                 단판 승부!                   ★\n");
-		gotoxy(37, 10);
-		printf("★");
-		gotoxy(85, 10);
-		printf("★");
-		gotoxy(37, 11);
-		printf("★");
-		gotoxy(85, 11);
-		printf("★");
-		gotoxy(37, 12);
-		printf("★  ESC를 누르면 다시 게임선택으로 돌아갑니다!  ★ ");
-		gotoxy(37, 13);
-		printf("★          이름은 6자 이내로 지어주세요!       ★");
-		gotoxy(37, 14);
-		printf("★★★★★★★★★★★★★★★★★★★★★★★★★\n");
-		do {
+enum KEYBOARD
+{
+	UP = 72,
+	LEFT = 75,
+	RIGHT = 77,
+	DOWN = 80
+};
+
+// 게임 시작 메인 함수
+void pushpull(void) {
+	system("cls");
+	while (1) {
+		switch (game()) {
+		case GAMESTART:
 			x = 40;
-			start_game(x, y);
-			/*
-			key = _getch();
-			if (key == ESC) {
-				exit(0);
-				return 0;
-			}*/
-			do {
-				game_fun();
-				} while (key != ESC);
-			} while (key != ESC);
-	} while (key != ESC);
+			start_game();
+			break;
+
+		case RULE:
+			menu_draw();
+			break;
+
+		case EXIT:
+			if (key == ENTER) exit(0);
+			return 0;
+		}
+	}
+	return 0;
+}
+
+// 게임 설명 함수
+void menu_draw() {
+	system("cls");
+	gotoxy(37, 5);
+	printf("★★★★★★★★★★ 줄다리기 ★★★★★★★★★");
+	printf("★");
+	gotoxy(37, 7);
+	printf("★         A키와 L키를 마구마구 연타!!          ★");
+	gotoxy(37, 9);
+	printf("★ 줄을 가장 빨리 자기쪽으로 당기는 게임입니다! ★");
+	gotoxy(37, 11);
+	printf("★                 단판 승부!                   ★");
+	gotoxy(37, 13);
+	printf("★   아무키나 눌러서 메뉴화면으로 돌아가세요    ★");
+	gotoxy(37, 15);
+	printf("★★★★★★★★★★★★★★★★★★★★★★★★★");
+
+	char kk = _getch();
+}
+
+// 게임 메인화면 함수
+void main_show() {
+	system("cls");
+	gotoxy(36, 10);
+	printf("★★★★★★★★★★ 줄다리기 ★★★★★★★★★★");
+
+	gotoxy(28, 10);
+	printf("●");
+	gotoxy(27, 11);
+	printf("＼|/");
+	gotoxy(28, 12);
+	printf(" |");
+	gotoxy(28, 13);
+	printf("/＼");
+
+	gotoxy(90, 10);
+	printf(" ●");
+	gotoxy(89, 11);
+	printf("＼|/");
+	gotoxy(90, 12);
+	printf(" |");
+	gotoxy(90, 13);
+	printf("/＼");
+
+	gotoxy(56, 13);
+	printf("게임 시작");
+
+	gotoxy(56, 15);
+	printf("게임 방법");
+	
+	gotoxy(56, 17);
+	printf("게임 종료");
+
+}
+
+// 메뉴 선택 커서 함수
+enum MENU game()
+{
+	int y = 0; 
+	int input = 0;
+	while (1){
+		main_show();
+
+		if (y <= 0)        
+		{
+			y = 0;
+		}
+		else if (y >= 6)
+		{
+			y = 2;
+		}
+
+		gotoxy(53, 13 + y); // 커서 메뉴에 맞춰서 위치조정
+		printf(">");
+
+		input = _getch();    //키보드로 하나의 키 입력
+
+		if (input == MAGIC_KEY) //방향키를 눌렀을 경우 -> 방향키 판별
+		{
+			switch (_getch())
+			{
+			case UP:            //방향키 위
+				gotoxy(53, 13 + y);
+				printf("  ");
+				y = y - 2;
+				break;
+			case DOWN:            //방향키 아래
+				gotoxy(53, 13 + y);
+				printf("  ");
+				y = y + 2;
+				break;
+			}
+		}
+		else if (input == ENTER) //enter 눌렀을 경우
+		{
+			switch (y)            
+			{
+			case 0:   
+				return GAMESTART;
+			case 2:    
+				return RULE;
+			case 4:    
+				return EXIT;
+			}
+		}
+	}
 }
 
 
+// 줄다리기 그려주는 함수
 void line(int x, int y)
 {
 	system("cls");
@@ -105,6 +207,7 @@ void line(int x, int y)
 	gotoxy(42, 2);
 	printf("움직이지 않는다면 한영키를 바꿔보세요!");
 
+	// x,y함수로 line그려줌 x =40, y = 11
 	gotoxy(x, y);
 	for (i = 0; i < 21; i++)
 		if (line[i] == 0)
@@ -124,37 +227,38 @@ void line(int x, int y)
 	gotoxy(45, 19);
 	printf("메뉴로 돌아가려면 ESC를 눌러주세요\n");
 
-	char menu_key = _getch();
-	if (menu_key == ESC) {
-		pushpull();
-	}
-
-}
-
-void startgame() {
+	//char menu_key = _getch();
+	//if (menu_key == ESC) {
+	//	pushpull();
+	//}
 
 }
 
 void start_game() {
-
-	gotoxy(47, 17);
+	system("cls");
+	gotoxy(47, 10);
 	printf("시작 전 팀 이름을 정해주세요!\n");
-	gotoxy(47, 18);
+	gotoxy(47, 12);
 	printf("첫번째 팀 이름입력 후 엔터!> ");
-	scanf_s("%s", &t_name1,20);
-	gotoxy(47, 19);
+	scanf_s("%s", &t_name1, 20);
+	gotoxy(47, 14);
 	printf("두번째 팀 이름입력 후 엔터!> ");
 	scanf_s("%s", &t_name2, 20);
+
+	// 이름 입력 후 화면 지우기
 	system("cls");
+	//line 그려줌
 	line(x, y);
+	//게임함수 실행
+	game_fun();
 }
 
 void game_fun() {
-	char click = _getch();
-	if (click == ESC) {
-		pushpull();
-	}
-	switch (click) {
+	while (1) {
+		char click = _getch();
+
+		switch (click) {
+			// 한칸가고 다시 돌아오고 에러
 		case 97: case 65: // A
 			x -= 2;
 			line(x, y);
@@ -167,6 +271,7 @@ void game_fun() {
 				Sleep(2000);
 				pushpull();
 			}
+			break;
 
 		case L: case l: // L
 			x += 2;
@@ -180,6 +285,11 @@ void game_fun() {
 				Sleep(2000);
 				pushpull();
 			}
+			break;
+		case ESC:
+			pushpull();
+			break;
 		}
+	}
 }
 	
